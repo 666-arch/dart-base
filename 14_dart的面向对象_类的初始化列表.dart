5355e5df76551f42bab86653971864c383cf49d2
MaxHuang <hc18174419502@163.com>
2024-07-25 10:36:23 +0800

    init 初始化列表

--- 14_dart的面向对象_类的初始化列表.dart
+++ 14_dart的面向对象_类的初始化列表.dart
@@ -1,3 +1,12 @@
 void main(List<String> args) {
-  
+  var p = Person('huang');
+
+}
+
+class Person {
+  final String? name;
+  // int? age = 20; //初始化值（不建议）
+  final int? age;
+
+  Person(this.name, {int? age}): this.age = age ?? 20; //初始化列表（建议）
 }
