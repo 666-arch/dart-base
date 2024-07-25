a5143a90fd7e199f29b199205cc8e29e8f040b07
MaxHuang <hc18174419502@163.com>
2024-07-25 11:00:51 +0800

    构造函数重定向

--- 14_dart的面向对象_类的初始化列表.dart
+++ 14_dart的面向对象_类的初始化列表.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
-  var p = Person('huang');
-
+  var p = Person('huang', age:10);
+  print(p.age);
 }
 
 class Person {
@@ -8,5 +8,6 @@ class Person {
   // int? age = 20; //初始化值（不建议）
   final int? age;
 
-  Person(this.name, {int? age}): this.age = age ?? 20; //初始化列表（建议）
+  Person(this.name, {int? age}): this.age = age ?? 20; //初始化列表（建议）写法一
+  // Person(this.name, {this.age = 20}); //写法二
 }
---
+++ 15_dart的面向对象_重定向构造函数.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var p1 = Person('huang');
+  print(p1.age);
+}
+
+class Person {
+  String? name;
+  int? age;
+
+  // Person(this.name): age = 0;
+
+  //构造函数的重定向
+  Person(String name): this._internal(name, 0);
+  Person._internal(this.name, this.age);
+}
