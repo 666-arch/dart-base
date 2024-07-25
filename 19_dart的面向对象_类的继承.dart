49e0321c4c57034810c39b48beef79d8a68e4851
MaxHuang <hc18174419502@163.com>
2024-07-25 15:03:58 +0800

    类的继承

--- 19_dart的面向对象_类的继承.dart
+++ 19_dart的面向对象_类的继承.dart
@@ -1,3 +1,15 @@
 void main(List<String> args) {
   
 }
+
+class Animal {
+  int? age;
+  Animal(this.age);
+}
+
+class Person extends Animal {
+  String? name;
+
+  //子类调用父类的构造函数需要通过 super 调用
+  Person(this.name, int age): super(age);
+}
